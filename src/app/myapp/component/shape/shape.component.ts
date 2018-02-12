7a98b7105d117da7c72ea54ff1482706dfa61ec3
lv-wei <lvw@coinv.com>
2018-02-12 17:32:36 +0800

    绘制了一个桥的基本模型

--- src/app/myapp/component/shape/shape.component.ts
+++ src/app/myapp/component/shape/shape.component.ts
@@ -1,53 +1,58 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild, AfterViewInit} from "@angular/core";
 import * as THREE from "three";
+import {SceneComponent} from "../scene/scene.component";
 
 @Component({
   selector: 'app-shape',
   templateUrl: './shape.component.html',
   styleUrls: ['./shape.component.css']
 })
-export class ShapeComponent implements OnInit {
+export class ShapeComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor() {
+  }
 
-  @ViewChild("content")
-  container: ElementRef;
+  rectMesh;
+
+  @ViewChild(SceneComponent)
+  scene: SceneComponent;
 
   ngOnInit() {
-    // Our Javascript will go here.
-    let scene = new THREE.Scene();
-    let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-    let renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    this.container.nativeElement.appendChild(renderer.domElement);
-
-
     var geometry = new THREE.Geometry();
-
     geometry.vertices.push(
-      new THREE.Vector3( -10,  10, 0 ),
-      new THREE.Vector3( -10, -10, 0 ),
-      new THREE.Vector3(  10, -10, 0 )
+      new THREE.Vector3(-30, 20, 0),
+      new THREE.Vector3(-30, 10, 0),
+      new THREE.Vector3(-20, 20, 10),
+      new THREE.Vector3(-20, 10, 10),
+      new THREE.Vector3(20, 20, 10),
+      new THREE.Vector3(20, 10, 10),
+      new THREE.Vector3(30, 20, 0),
+      new THREE.Vector3(30, 10, 0),
     );
-
-    geometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
-
+    geometry.faces.push(new THREE.Face3(0, 1, 2));
+    geometry.faces.push(new THREE.Face3(1, 3, 2));
+    geometry.faces.push(new THREE.Face3(2, 3, 4));
+    geometry.faces.push(new THREE.Face3(3, 5, 4));
+    geometry.faces.push(new THREE.Face3(4, 5, 6));
+    geometry.faces.push(new THREE.Face3(5, 7, 6));
     geometry.computeBoundingSphere();
 
-    var rectMesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: 0xff0000 } ) ) ;
+    let material = new THREE.MeshBasicMaterial({color: 0xff0000, morphTargets: false});
+    material.side = THREE.DoubleSide;
 
-    scene.add( rectMesh );
+    this.rectMesh = new THREE.Mesh(geometry, material);
+  }
 
-    camera.position.z = 5;
-
-    let render = function () {
+  ngAfterViewInit(): void {
+    let render = () => {
       requestAnimationFrame(render);
 
-      renderer.render(scene, camera);
+      // this.rectMesh.rotation.x += 0.1;
+      // this.rectMesh.rotation.y += 0;
+
+      this.scene.render();
     };
 
     render();
   }
-
 }
