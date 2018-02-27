be7a68771b8a5b5b89002ba5f8d3fdf0dfd5427c
lv-wei <lvw@coinv.com>
2018-02-27 15:01:15 +0800

    bridge增加变形效果

--- src/app/myapp/example/draw-bridge/draw-bridge.component.ts
+++ src/app/myapp/example/draw-bridge/draw-bridge.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from "@angular/core";
 import {SceneComponent} from "../../component/scene/scene.component";
 import * as THREE from "three";
+import {Vector3} from "three";
 
 @Component({
   selector: 'app-draw-bridge',
@@ -9,52 +10,110 @@ import * as THREE from "three";
 })
 export class DrawBridgeComponent implements OnInit {
 
-
   constructor() {
   }
 
+  vertices: Array<Array<Vector3>> = [
+    [
+      new THREE.Vector3(-30, 20, 0),
+      new THREE.Vector3(-20, 20, 10),
+      new THREE.Vector3(-7, 20, 10),
+      new THREE.Vector3(6, 20, 10),
+      new THREE.Vector3(20, 20, 10),
+      new THREE.Vector3(30, 20, 0),
+      new THREE.Vector3(-30, 10, 0),
+      new THREE.Vector3(-20, 10, 10),
+      new THREE.Vector3(-7, 10, 10),
+      new THREE.Vector3(6, 10, 10),
+      new THREE.Vector3(20, 10, 10),
+      new THREE.Vector3(30, 10, 0),
+    ],
+    [
+      new THREE.Vector3(-30, 20, 0),
+      new THREE.Vector3(-20, 20, 10),
+      new THREE.Vector3(-7, 20, 8),
+      new THREE.Vector3(6, 20, 10),
+      new THREE.Vector3(20, 20, 10),
+      new THREE.Vector3(30, 20, 0),
+      new THREE.Vector3(-30, 10, 0),
+      new THREE.Vector3(-20, 10, 10),
+      new THREE.Vector3(-7, 10, 8),
+      new THREE.Vector3(6, 10, 10),
+      new THREE.Vector3(20, 10, 10),
+      new THREE.Vector3(30, 10, 0),
+    ],
+    [
+      new THREE.Vector3(-30, 20, 0),
+      new THREE.Vector3(-20, 20, 10),
+      new THREE.Vector3(-7, 20, 10),
+      new THREE.Vector3(6, 20, 12),
+      new THREE.Vector3(20, 20, 10),
+      new THREE.Vector3(30, 20, 0),
+      new THREE.Vector3(-30, 10, 0),
+      new THREE.Vector3(-20, 10, 10),
+      new THREE.Vector3(-7, 10, 10),
+      new THREE.Vector3(6, 10, 12),
+      new THREE.Vector3(20, 10, 10),
+      new THREE.Vector3(30, 10, 0),
+    ]
+  ];
+
   rectMesh;
+  geometry;
 
   @ViewChild(SceneComponent)
   scene: SceneComponent;
 
   ngOnInit() {
-    var geometry = new THREE.Geometry();
-    geometry.vertices.push(
-      new THREE.Vector3(-30, 20, 0),
-      new THREE.Vector3(-30, 10, 0),
-      new THREE.Vector3(-20, 20, 10),
-      new THREE.Vector3(-20, 10, 10),
-      new THREE.Vector3(20, 20, 10),
-      new THREE.Vector3(20, 10, 10),
-      new THREE.Vector3(30, 20, 0),
-      new THREE.Vector3(30, 10, 0),
-    );
-    geometry.faces.push(new THREE.Face3(0, 1, 2));
-    geometry.faces.push(new THREE.Face3(1, 3, 2));
-    geometry.faces.push(new THREE.Face3(2, 3, 4));
-    geometry.faces.push(new THREE.Face3(3, 5, 4));
-    geometry.faces.push(new THREE.Face3(4, 5, 6));
-    geometry.faces.push(new THREE.Face3(5, 7, 6));
-    geometry.computeBoundingSphere();
+    this.geometry = new THREE.Geometry();
+    // this.geometry.verticesNeedUpdate = true;
+    // this.geometry.elementsNeedUpdate = true;
 
-    let material = new THREE.MeshBasicMaterial({color: 0xff0000, morphTargets: false});
+    this.geometry.vertices = this.vertices[0];
+
+    this.geometry.faces.push(new THREE.Face3(1, 0, 6));
+    this.geometry.faces.push(new THREE.Face3(1, 6, 7));
+    this.geometry.faces.push(new THREE.Face3(2, 1, 7));
+    this.geometry.faces.push(new THREE.Face3(2, 7, 8));
+    this.geometry.faces.push(new THREE.Face3(3, 2, 8));
+    this.geometry.faces.push(new THREE.Face3(3, 8, 9));
+    this.geometry.faces.push(new THREE.Face3(4, 3, 9));
+    this.geometry.faces.push(new THREE.Face3(4, 9, 10));
+    this.geometry.faces.push(new THREE.Face3(5, 4, 10));
+    this.geometry.faces.push(new THREE.Face3(5, 10, 11));
+    this.geometry.computeBoundingSphere();
+
+    for (let i = 1; i < this.vertices.length; i++) {
+      this.geometry.morphTargets.push({name: "target" + i, vertices: this.vertices[i]});
+    }
+
+    let material = new THREE.MeshBasicMaterial({color: 0x00ff00, morphTargets: true});
+    // let material = new THREE.MeshLambertMaterial({color: 0xff0000, morphTargets: true});
     material.side = THREE.DoubleSide;
 
-    this.rectMesh = new THREE.Mesh(geometry, material);
+    this.rectMesh = new THREE.Mesh(this.geometry, material);
+
   }
 
   ngAfterViewInit(): void {
-    let render = () => {
-      requestAnimationFrame(render);
+    let weight = 0; // morph权重
+    let clock = new THREE.Clock();
+    let ratio: number = 10; // 变形频率
+    let amplitude: number = 2; // 变形幅度
 
-      // this.rectMesh.rotation.x += 0.1;
-      // this.rectMesh.rotation.y += 0;
+    let animate = () => {
+      requestAnimationFrame(animate);
+
+      weight = Math.sin(clock.getElapsedTime() * ratio) * amplitude;
+
+      for (let i = 0; i < this.vertices.length - 1; i++) {
+        this.rectMesh.morphTargetInfluences[i] = weight;
+      }
 
       this.scene.render();
     };
 
-    render();
+    animate();
   }
 
 }
